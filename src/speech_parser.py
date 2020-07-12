309f1db6d83a87fa622121b47ae544d021be20f1
Varun Lakshmanan <varunlakshmanan@outlook.com>
2020-07-11 23:55:30 -0400

    Finished extract_information method

--- src/speech_parser.py
+++ src/speech_parser.py
@@ -1,4 +1,5 @@
 import speech_recognition as sr
+from transformers import pipeline
 
 
 def speech_to_text(audio_file_path):
@@ -10,3 +11,27 @@ def speech_to_text(audio_file_path):
             return transcript
         except:
             return "Google Web Speech API not accessible"
+
+
+def extract_information(transcript):
+    name_location_organization_extractor = pipeline("ner")
+    names_locations_organizations = name_location_organization_extractor(transcript)
+    names = []
+    locations = []
+    organizations = []
+    for entry in names_locations_organizations:
+        if entry['score'] >= 0.9:
+            if entry['name'] == 'B-PER' or entry['name'] == 'I-PER':
+                names.append(entry['word'])
+            elif entry['entity'] == 'B-LOC' or entry['entity'] == 'I-LOC':
+                locations.append(entry['word'])
+            elif entry['entity'] == 'B-ORG' or entry['entity'] == 'I-ORG':
+                organizations.append(entry['word'])
+
+    summary_extractor = pipeline("summarization")
+    summary = summary_extractor(transcript, max_length=100)
+
+    urgency_extractor = pipeline("sentiment-analysis")
+    urgency = urgency_extractor(transcript)[0]
+
+    return names, locations, organizations, summary, urgency
